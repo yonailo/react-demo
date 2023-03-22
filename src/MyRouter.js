66f53fc191ecd81b9a36cda8062f9e3395fb02ab
yonailo <juanfrancisco.rodriguezhervella@atos.net>
2023-03-22 08:55:59 +0000

    See: https://github.com/rafgraph/spa-github-pages

--- src/MyRouter.js
+++ src/MyRouter.js
@@ -7,40 +7,43 @@ import RegisterScreen from "./Screens/RegisterScreen";
 import DashboardScreen from "./Screens/DashboardScreen";
 import CreatePostScreen from "./Screens/CreatePostScreen";
 import ListPostsScreen from "./Screens/ListPostsScreen";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Homepage />,
-  },
-  {
-    path: "/counter",
-    element: <Counter />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/planet",
-    element: <PlanetScreen />,
-  },
-  {
-    path: "/register",
-    element: <RegisterScreen />,
-  },
-  {
-    path: "/dashboard",
-    element: <DashboardScreen />,
-  },
-  {
-    path: "/createPost",
-    element: <CreatePostScreen />,
-  },
-  {
-    path: "/listPosts",
-    element: <ListPostsScreen />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Homepage />,
+    },
+    {
+      path: "/counter",
+      element: <Counter />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/planet",
+      element: <PlanetScreen />,
+    },
+    {
+      path: "/register",
+      element: <RegisterScreen />,
+    },
+    {
+      path: "/dashboard",
+      element: <DashboardScreen />,
+    },
+    {
+      path: "/createPost",
+      element: <CreatePostScreen />,
+    },
+    {
+      path: "/listPosts",
+      element: <ListPostsScreen />,
+    },
+  ],
+  { basename: "/react-demo" }
+);
 
 function MyRouter() {
   return <RouterProvider router={router} />;
